9d0e343c1f41099fa77dfbdb330c78699ec220eb
gyankov <g.yankov951@abv.bg>
2017-03-17 13:29:08 +0200

    Only view engine is razor now

--- Goomer/Goomer.Web/Global.asax.cs
+++ Goomer/Goomer.Web/Global.asax.cs
@@ -13,12 +13,17 @@ namespace Goomer.Web
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
             Database.SetInitializer<ApplicationDbContext>(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             ApplicationDbContext.Create().Database.Initialize(true);
+
             var autoMapperConfig = new AutoMapperConfig();
             autoMapperConfig.Execute(Assembly.GetExecutingAssembly());
         }
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Goomer
-Goomer
 [![Build status](https://ci.appveyor.com/api/projects/status/gqbnw8lb9cxmrg1m?svg=true)](https://ci.appveyor.com/project/gyankov/goomer)
