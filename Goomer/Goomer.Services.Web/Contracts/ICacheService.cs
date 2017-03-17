b052a48c1bc46bc2519ea15f1370a3b2fd165a69
gyankov <g.yankov951@abv.bg>
2017-03-17 09:57:26 +0200

    Cache service added

---
+++ Goomer/Goomer.Services.Web/Contracts/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goomer.Services.Web.Contracts
+{
+    public interface ICacheService
+    {
+        T Get<T>(string itemName, Func<T> getDataFunc, int durationInSeconds);
+
+        void Remove(string itemName);
+    }
+}
---
+++ Goomer/Goomer.Services.Web/HttpCacheService.cs
@@ -0,0 +1,39 @@
+﻿using Goomer.Services.Web.Contracts;
+using System;
+using System.Web;
+using System.Web.Caching;
+
+namespace Goomer.Services.Web
+{
+    public class HttpCacheService : ICacheService
+    {
+        private static readonly object LockObject = new object();
+
+        public T Get<T>(string itemName, Func<T> getDataFunc, int durationInSeconds)
+        {
+            if (HttpRuntime.Cache[itemName] == null)
+            {
+                lock (LockObject)
+                {
+                    if (HttpRuntime.Cache[itemName] == null)
+                    {
+                        var data = getDataFunc();
+                        HttpRuntime.Cache.Insert(
+                            itemName,
+                            data,
+                            null,
+                            DateTime.UtcNow.AddSeconds(durationInSeconds),
+                            Cache.NoSlidingExpiration);
+                    }
+                }
+            }
+
+            return (T)HttpRuntime.Cache[itemName];
+        }
+
+        public void Remove(string itemName)
+        {
+            HttpRuntime.Cache.Remove(itemName);
+        }
+    }
+}
