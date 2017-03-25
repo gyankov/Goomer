fe12ec2f8d5cdf31a612fb0f3c2738d6bd8b172b
Georgi <g.yankov951@abv.bg>
2017-03-25 15:30:08 +0200

    Added kendo

--- Goomer/Goomer.Web/App_Start/BundleConfig.cs
+++ Goomer/Goomer.Web/App_Start/BundleConfig.cs
@@ -8,8 +8,16 @@ namespace Goomer.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include("~/Content/Kendo/kendo.common.*", 
+                "~/Content/Kendo/kendo.default.*",
+                "~/Content/kendo/kendo.bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/Kendo/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include("~/Scripts/Kendo/kendo.all.min.js", "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
